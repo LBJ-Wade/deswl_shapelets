e849fa12e6e7ff81ab3a7b7202f890db942f2b7a
Erin Sheldon <erin.sheldon@gmail.com>
2009-05-01 04:02:39 +0000

    bug with version finding
    git-svn-id: https://desweb.cosmology.illinois.edu/svn/desdm/devel/wl/trunk@3535 2422e80f-9519-0410-a819-8e21e7c86b6d

--- python/__init__.py
+++ python/__init__.py
@@ -1,22 +1,32 @@
 # Don't edit these svn properties by hand
 _property_headurl='$HeadURL$'
 
-#_property_rev='$Rev: 1339 $'
-
-#def GetRevision():
-#    revision = _property_rev.split()[1]
-#    return revision
-
 def GetWlVersion():
-    import re
+    import os
+    from sys import stderr
 
     thisname='/python/__init__.py'
-    headstrip=re.compile( '^\$HeadURL: .*/trunk/' )
-    stripped_headurl = headstrip.sub('trunk/', _property_headurl)
-    tag = stripped_headurl.replace(' $','')
-    tag = tag.replace(thisname,'')
+    badvers="NOTAG: unparseable"
 
+    psplit=_property_headurl.split()
+    if len(psplit) != 3:
+        mess="headurl did not split into 3: '%s'\n" % _property_headurl
+        stderr.write(mess)
+        return badvers
+
+    url=psplit[1]
+
+    if url.find(thisname) == -1:
+        mess="url '%s' does not contain string '%s'\n" % \
+                (_property_headurl, thisname)
+        stderr.write(mess)
+        return badvers
+
+    urlfront = url.replace(thisname, '')
+
+    tag = os.path.basename(urlfront)
     return tag
+
 
 version=GetWlVersion()
 import wlpipe
