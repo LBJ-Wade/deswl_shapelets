7b34f985b5af83e63a2110fc2eb330bb8b3a742c
Erin Sheldon <erin.sheldon@gmail.com>
2013-03-22 03:18:46 +0000

    added iterator for config file
    git-svn-id: https://desweb.cosmology.illinois.edu/svn/desdm/devel/wl/trunk@11250 2422e80f-9519-0410-a819-8e21e7c86b6d

--- src/ConfigFile.h
+++ src/ConfigFile.h
@@ -383,6 +383,11 @@ public:
                 const std::string include = "+",
                 const std::string sentry = "EndConfigFile" );
 
+    std::map<std::string,ConvertibleString>::const_iterator
+    begin() const {return _contents.begin(); }
+    std::map<std::string,ConvertibleString>::const_iterator
+    end() const {return _contents.end(); }
+
     // Load more value from a file.
     void load( const std::string file_name );
     //{ std::ifstream fs(file_name.c_str()); read(fs); }
